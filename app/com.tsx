a67b2aa5374a08e7b76d60e0420564428d5fd2b2
connectwithfalco <faisal.saleem@universalplus.technology>
2026-04-16 22:39:33 +0500

    code

--- app/com.tsx
+++ app/com.tsx
@@ -1,23 +1,18 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 export default function Page2() {
-    const ref = useRef<HTMLInputElement>(null);
 
-    const triggerHaptic = () => {
-        ref.current?.click(); // fires real iOS haptic
-    };
-
-    const foursec = () => {
-        setTimeout(triggerHaptic, 4000);
-    };
+    useEffect(() => {
+        setTimeout(() => {
+            Haptics.impact({ style: ImpactStyle.Heavy }); // ✅ works after delay
+        }, 4000);
+    }, [])
 
     return (
         <>
-            <label style={{ position: "absolute", left: -9999 }}>
-                <input ref={ref} type="checkbox" /* @ts-ignore */ />
-            </label>
-            <button onClick={foursec}>Vibrate after 4 sec</button>
+           
         </>
     );
 }
